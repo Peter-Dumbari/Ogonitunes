648f4df3d5a917765c570a4ddfe38f6013266a50
Peter Dumbari <dummytwiz@gmail.com>
2026-02-14 14:32:43 +0100

    deleted a file the was stopping deployment

--- src/app/sitemap.js
+++
@@ -1,8 +0,0 @@
-export default async function sitemap() {
-  const songs = await getAllSongs();
-
-  return songs.map((song) => ({
-    url: `https://ogonitunes.com/song/${song.slug}`,
-    lastModified: new Date(),
-  }));
-}
